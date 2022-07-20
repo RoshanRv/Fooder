d2f591541a3d47fde20745a46757c93ad6a69293
Roshan <nkroshankumar@gmail.com>
2022-07-20 20:49:16 +0530

    homehero updated

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -24,15 +24,15 @@ const HomeHero = () => {
             <div className='flex w-max  transition-all duration-700' style={{transform:`translateX(${-slideIndex*100}vw)`}} >
             {[1,2,3].map((data:number,i)=>(
                     <div key={i} className='w-screen h-[50rem] ' >
-                        <img  src={`/img/banner${data}.png`}  className='w-full h-full' />
+                        <img  src={`/img/banner${data}.png`}  className='w-full h-full lg:object-fill object-center object-cover' />
                     </div>
                 ))}  
             </div>
         </section>
         {/* left */}
-        <button className='z-50 fixed top-1/2  -translate-y-1/2 left-10' onClick={()=>handlePrevious()} ><FontAwesomeIcon icon={faAngleLeft} className='w-10 text-white h-12 rounded-full bg-white/20 p-2 z-50 px-2' /></button>
+        <button className='z-50 fixed top-1/2  -translate-y-1/2 md:left-10 left-4' onClick={()=>handlePrevious()} ><FontAwesomeIcon icon={faAngleLeft} className='w-10 text-white h-12 rounded-full bg-white/20 p-2 z-50 px-2' /></button>
         {/* right */}
-        <button className='z-50 fixed top-1/2 -translate-y-1/2 left-[94vw]' onClick={()=>handleNext()} ><FontAwesomeIcon icon={faAngleRight} className='w-10 text-white h-12 rounded-full bg-white/20 p-2 z-50 px-2' /></button>
+        <button className='z-50 fixed top-1/2 -translate-y-1/2 md:right-10 right-4' onClick={()=>handleNext()} ><FontAwesomeIcon icon={faAngleRight} className='w-10 text-white h-12 rounded-full bg-white/20 p-2 z-50 px-2' /></button>
     </section>
    
   )
