5715581855b65d193ae92530ff6261dc40d86806
Roshan <nkroshankumar@gmail.com>
2022-07-20 20:55:33 +0530

    added product list

---
+++ components/ProductList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+    <section className='my-4  mt-20 text-center'>
+        <h1 className="lg:text-6xl text-4xl my-8 font-black font-serif text-rose-800">Our Pizza Varities</h1>
+        <p className='font-light text-xl text-left lg:mx-16 mx-4' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, enim suscipit inventore et consequatur fugiat ipsa natus similique dolore quis laborum nam libero voluptatibus itaque quam omnis ut dolor nihil ullam reprehenderit accusantium odio ad? Repellat quam laboriosam quaerat cupiditate, officia accusamus unde necessitatibus, labore non impedit ipsa asperiores explicabo.</p>
+    </section>
+  )
+}
+
+export default ProductList
