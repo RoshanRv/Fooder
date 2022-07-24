8f17fe3e5a19048779e0d884215cdaf53792d607
Roshan <nkroshankumar@gmail.com>
2022-07-24 09:00:25 +0530

    test ...

--- pages/api/order/index.ts
+++ pages/api/order/index.ts
@@ -2,34 +2,34 @@ import Order from "../../../modals/Order";
 import dbConnect from "../../../utlis/mongo";
 
 const handler = async (req:any,res:any)=>{
-    const {method} = req
+    // const {method} = req
 
     try{
-        console.log('Check for errror')
+        // console.log('Check for errror')
         dbConnect()
         res.json('No err da...')
     }catch(err){
-        res.json(err,'oops')
+        res.json('oops')
     }
 
-    if(method=='GET'){
-        try{
-            const order = await Order.find()
-            res.status(201).json(order)
-        }catch(err){
-            res.status(500).json(err)
-        }
-    }  
+    // if(method=='GET'){
+    //     try{
+    //         const order = await Order.find()
+    //         res.status(201).json(order)
+    //     }catch(err){
+    //         res.status(500).json(err)
+    //     }
+    // }  
 
-    if(method=='POST'){
-        try{
-            const order = await Order.create(req.body)
-            console.log(order)
-            res.status(201).json(order)
-        }catch(err){
-            res.status(500).json(err)
-        }
-    }  
+    // if(method=='POST'){
+    //     try{
+    //         const order = await Order.create(req.body)
+    //         console.log(order)
+    //         res.status(201).json(order)
+    //     }catch(err){
+    //         res.status(500).json(err)
+    //     }
+    // }  
 }
 
 export default handler
